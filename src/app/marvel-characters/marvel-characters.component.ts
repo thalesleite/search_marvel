6fb5031fe880b610061788a3b215220a35f21c01
ThalesLeite <thales.assuncao@gmail.com>
2018-09-29 13:07:53 -0300

    configuration SASS and iniciate build the default layout

---
+++ src/app/marvel-characters/marvel-characters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marvel-characters',
+  templateUrl: './marvel-characters.component.html',
+  styleUrls: ['./marvel-characters.component.scss']
+})
+export class MarvelCharactersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/marvel/marvel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marvel',
+  templateUrl: './marvel.component.html',
+  styleUrls: ['./marvel.component.scss']
+})
+export class MarvelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
